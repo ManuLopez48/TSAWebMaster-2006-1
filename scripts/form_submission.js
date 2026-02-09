fbffaba4263d1e97ed4df00d0bf3b5cdb80fee8e
ManuLopez48 <mruizlopez451@gmail.com>
2026-02-08 17:22:08 -0700

    Finished Laying out the form submission; functionality on form_submission.js is being implemented.

--- scripts/form_submission.js
+++ scripts/form_submission.js
@@ -1,6 +1,22 @@
+// Info & Contact Elements
+const submissionName = document.getElementById('form-addition-name');
+const organizationName = document.getElementById('form-organization-name');
+const contactEmail = document.getElementById('form-organization-contact-email');
+const contactPhone = document.getElementById('form-organization-contact-phone');
+
+// Image Elements
+
+//Summary Elements
+const mainSummary = document.getElementById('form-summary');
+const shortSummary = document.getElementById('form-short-summary');
+
+// Date Elements
 const rangedDateToggle = document.getElementById('form-event-date-toggle');
 const formSingleDateDiv = document.getElementById('form-single-date');
 const formMultiDateDiv = document.getElementById('form-multi-date');
+
+// Submission Button
+const submitButton = document.getElementById('submit-button')
 
 document.addEventListener('DOMContentLoaded', () => {
     checkDateSetup();
@@ -19,3 +35,4 @@ function checkDateSetup() {
         formMultiDateDiv.hidden = true;
     }
 }
+
