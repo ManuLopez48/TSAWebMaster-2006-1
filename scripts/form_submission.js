f7c9615bf1682ceefd98cae13640b1f8a4979632
ManuLopez48 <mruizlopez451@gmail.com>
2026-02-12 21:45:52 -0700

    Created Image Detection for form submission. WORK IN PROGRESS

--- scripts/form_submission.js
+++ scripts/form_submission.js
@@ -5,7 +5,8 @@ const contactEmail = document.getElementById('form-organization-contact-email');
 const contactPhone = document.getElementById('form-organization-contact-phone');
 
 // Image Elements
-
+const organizationLogo = document.getElementById('form-add-image-input-organization');
+const eventImg = document.getElementById('form-add-image-additional-input');
 
 //Summary Elements
 const mainSummary = document.getElementById('form-summary');
@@ -46,3 +47,36 @@ submitButton.addEventListener('click', () => {
     }
 });
 
+organizationLogo.addEventListener('change', (event) => {
+    const submitted = event.target;
+
+    if(submitted.files && submitted.files[0]) {
+        const organizationIMGUploaded = document.getElementById('og-img-uploaded');
+        const url = URL.createObjectURL(submitted.files[0]);
+
+        organizationIMGUploaded.src = url;
+        organizationIMGUploaded.style.display = 'block';
+        organizationIMGUploaded.style.width = '100px';
+        organizationIMGUploaded.style.height = '100px';
+
+        organizationIMGUploaded.onload = () => URL.revokeObjectURL(url);
+    }
+    console.log('Organization Submit Working');
+});
+
+eventImg.addEventListener('change', (event) => {
+    const submitted = event.target;
+
+    if(submitted.files && submitted.files[0]) {
+        const organizationIMGUploaded = document.getElementById('og-img-uploaded');
+        const url = URL.createObjectURL(submitted.files[0]);
+
+        organizationIMGUploaded.src = url;
+        organizationIMGUploaded.style.display = 'block';
+        organizationIMGUploaded.style.width = '100px';
+        organizationIMGUploaded.style.height = '100px';
+
+        organizationIMGUploaded.onload = () => URL.revokeObjectURL(url);
+    }
+    console.log('Img Submit Working');
+});
