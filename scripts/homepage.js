d201d538274ec9192ac2354940fe83c61dc3ef2c
ManuLopez48 <mruizlopez451@gmail.com>
2026-02-15 11:55:14 -0700

    Added Kelly Center Submission

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -3,3 +3,13 @@ const homepageSearchButton = document.getElementById('search-button');
 homepageSearchButton.addEventListener('click', () => {
     window.location.href = "/subpage/search.html"
 });
+
+const cardButton = document.getElementsByClassName('card-button');
+
+document.addEventListener('DOMContentLoaded', () => {
+    for(let i = 0; i < cardButton.length; i++) {
+        cardButton[i].addEventListener('click', (e) => {
+            window.location.href = e.currentTarget.dataset.link;
+        });
+    }
+});
